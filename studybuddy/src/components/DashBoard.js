d82db18497bfad22010f4dd79260199c519f00ae
Marc <hi@gmail.com>
2023-05-29 00:11:54 +0800

    Rebased and fix a bug

---
+++ studybuddy/src/components/DashBoard.js
@@ -0,0 +1,22 @@
+import { Card } from "@mui/material";
+import NavBar from "./NavBar";
+
+
+export default function DashBoard() {
+
+    return(
+        <>
+            <NavBar/>
+            
+            <Card>
+                Feature 1
+            </Card>
+
+            <Card>
+                Feature 2
+            </Card>
+        </>
+        
+    );
+
+}
